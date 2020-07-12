f40f16e4dc60a280befa4c1651fdeadc0fcf8431
DAP <dlrayjr89@gmail.com>
2020-07-12 02:21:42 +0000

    init commit

---
+++ backend/api/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+const server = express()
+
+server.use(express.json())
+
+
+server.get('/', (req,res) => {
+    res.status(200).json({message: 'Apii is up and running'})
+})
+
+module.exports = server
