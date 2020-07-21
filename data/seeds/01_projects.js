3dad1ebd5b375dc313a3a78b292924047dea2422
DAP <dlrayjr89@gmail.com>
2020-07-21 02:04:36 +0000

    update on migrations and seed

--- data/seeds/01_projects.js
+++ data/seeds/01_projects.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('projects').insert([
-        {id: 1, name: 'Fan Page', details: 'Create an application based off one of my favorite shows or anything thats i am interested in'},
-        {id: 2, name: 'Online Store', details: 'Design and develop an application that users can purchase goods from. '},
-        {id: 3, name: 'Learn Python', details: 'Take some time to learn Python. Learn the basic from diferent data types to collections. LEarn whats they mean and what they do'}
+        {id: 1, name: 'Fan Page', details: 'Create an application based off one of my favorite shows or anything thats i am interested in', createdAt: 'july, 20, 2020'},
+        {id: 2, name: 'Online Store', details: 'Design and develop an application that users can purchase goods from. ', createdAt:'july, 10, 2020'},
+        {id: 3, name: 'Learn Python', details: 'Take some time to learn Python. Learn the basic from diferent data types to collections. LEarn whats they mean and what they do', createdAt:'july, 15, 2020'}
       ]);
     });
 };
